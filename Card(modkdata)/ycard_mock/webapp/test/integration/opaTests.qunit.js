46ef6977aea9671b5d1aaa57e965f2e55e24d4ac
jaewon yim <jwimj1011@naver.com>
2022-07-05 11:54:43 +0900

    22.07.05 FULL SYNC

---
+++ Card(modkdata)/ycard_mock/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["CardApp/ycardmock/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ Card(modkdata)/ycard_mock/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"CardApp/ycard_mock/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
